8b2489f1645033eca05b76d4352cb3a196177723
BryceVerberne <bryceverberne@gmail.com>
2023-03-10 15:46:22 -0700

    Add Tic-Tac-Toe Project

--- Tic-Tac-Toe/src/TicTacToe.java
+++ Tic-Tac-Toe/src/TicTacToe.java
@@ -292,12 +292,3 @@ public class TicTacToe extends Board {
         }
     }
 }
-
-// Disclaimer:
-// The given assignment description, project files, code files and/or solution files
-// should not be made available in a public form via methods such as online hosting
-// in code repositories, educational resource hosting websites, etc. such as Course
-// Hero and/or Chegg. Tracking information is embedded into the assignment files and
-// any person found to be distributing files may be prosecuted. This includes
-// notification to the college, any discipline it warrants and legal action if
-// it is warranted.
